326e37db5504e0147a900998620f49030e4d88fb
Partitioners <aniketsingh.sachan@gmail.com>
2017-03-12 15:39:59 +0530

    Initialising project

---
+++ app/src/main/java/pl/hypeapp/endoscope/ui/activity/MainMenuActivity.java
@@ -0,0 +1,28 @@
+package pl.hypeapp.endoscope.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import pl.hypeapp.endoscope.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+    public void intentStreamVideo(View view) {
+        Intent i =  new Intent(MainMenuActivity.this, StartStreamActivity.class);
+        startActivity(i);
+    }
+    public void intentViewStream(View view) {
+        Intent i =  new Intent(MainMenuActivity.this, ConnectToStreamActivity.class);
+        startActivity(i);
+    }
+
+}
+
